17a10aca0ea1592dc7624a6d1b61b05e0218546d
Uyttersprot Pieter <Pieter.u@hotmail.com>
2016-12-30 11:38:59 +0100

    Created BlackJackGame & BlackJackGame.Tests

---
+++ BlackJackGame/src/BlackJackGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlackJackGame")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8667ee41-a3ca-46ce-b6df-f3d93a28bfc0")]
